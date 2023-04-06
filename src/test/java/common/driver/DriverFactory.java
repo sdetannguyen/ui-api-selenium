be6f27210d7c5937a4517e2811bd31ce665cc48c
Kirigan <axnguyen@Kirigans-MacBook-Pro.local>
2023-04-06 11:43:11 +0700

    Fix driver init issue https://stackoverflow.com/questions/75678572/java-io-ioexception-invalid-status-code-403-text-forbidden

--- src/test/java/common/driver/DriverFactory.java
+++ src/test/java/common/driver/DriverFactory.java
@@ -3,6 +3,7 @@ package common.driver;
 public class DriverFactory {
 
     public static DriverManager getManager(DriverType type) {
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
         DriverManager driverManager;
         switch (type) {
             //Init more driver types (Eg: Firefox, Safari...) here
